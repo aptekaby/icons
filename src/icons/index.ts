5af6b013564fc4ab690f76dffd41c51da159bd95
Pavel <solyqnik@gmail.com>
2025-07-30 15:09:31 +0300

    feat: настроил semantic release, добавил новую иконку

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,4 +1,5 @@
 import IconTrash from "./iconTrash/iconTrash.svg";
 import IconTrash2 from "./iconTrash2/iconTrash2.svg";
+import IconTrash3 from "./iconTrash3/iconTrash2.svg";
 
-export { IconTrash, IconTrash2 };
+export { IconTrash, IconTrash2, IconTrash3 };
