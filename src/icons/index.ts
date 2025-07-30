87eefa4c4ea4780edc40efc366d0f3d592f87248
Pavel <solyqnik@gmail.com>
2025-07-30 15:33:56 +0300

    ci: поменял release workflows

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,5 +1,6 @@
 import IconTrash from "./iconTrash/iconTrash.svg";
 import IconTrash2 from "./iconTrash2/iconTrash2.svg";
 import IconTrash3 from "./iconTrash3/iconTrash2.svg";
+import IconTrash4 from "./iconTrash3/iconTrash2.svg";
 
-export { IconTrash, IconTrash2, IconTrash3 };
+export { IconTrash, IconTrash2, IconTrash3, IconTrash4 };
