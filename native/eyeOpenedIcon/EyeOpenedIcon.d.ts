792a1949fbb704ce38399326230d56fad5e99ab3
sosnovichroman <sosnovichroman@gmail.com>
2025-12-22 16:59:12 +0300

    fix(v2): изменены иконки v2, добавлены новые

---
+++ native/eyeOpenedIcon/EyeOpenedIcon.d.ts
@@ -0,0 +1,4 @@
+import * as React from "react";
+import type { SvgProps } from "react-native-svg";
+declare const SvgEyeOpenedIcon: (props: SvgProps) => React.JSX.Element;
+export default SvgEyeOpenedIcon;
---
+++ native/favouriteWhiteBasedIcon/index.d.ts
@@ -0,0 +1,1 @@
+export { default as FavouriteWhiteBasedIcon } from "./FavouriteWhiteBasedIcon";
