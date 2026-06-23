2adb8ff8595e5474e4018a883c196963bd087946
sosnovichroman <sosnovichroman@gmail.com>
2026-06-23 12:14:02 +0300

    feat(chat unavailable): добавлена новая иконка "Чат недоступен"

--- web/index.d.ts
+++ web/index.d.ts
@@ -9,6 +9,7 @@ export { CartFilledIcon } from './cartFilledIcon';
 export { CartIcon } from './cartIcon';
 export { CatalogIcon } from './catalogIcon';
 export { ChatIcon } from './chatIcon';
+export { ChatUnavailableIcon } from './chatUnavailableIcon';
 export { ChatUnreadIcon } from './chatUnreadIcon';
 export { CheckboxIcon } from './checkboxIcon';
 export { CheckboxPressedIcon } from './checkboxPressedIcon';
